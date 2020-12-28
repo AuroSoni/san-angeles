30443bc9965d36f4c697865b72103914b00ab8d1
AuroSoni <aurokumar926@gmail.com>
2020-12-28 13:34:30 +0530

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/example/SanAngeles/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.SanAngeles;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.SanAngeles";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 4000001;
+  public static final String VERSION_NAME = "1.0";
+}
